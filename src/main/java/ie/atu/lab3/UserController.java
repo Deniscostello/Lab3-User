a8748d7e17a029593f27aced563dbed9badcda9a
Denis Costello <G00396309@atu.ie>
2023-10-05 14:22:01 +0100

    Added postmapping and @requestBody for "registerUser body" Added User class

--- src/main/java/ie/atu/lab3/UserController.java
+++ src/main/java/ie/atu/lab3/UserController.java
@@ -1,9 +1,8 @@
 package ie.atu.lab3;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -21,6 +20,13 @@ public class UserController {
     public String getUser(@PathVariable String name, @PathVariable String email){
         us.registerUser(name, email);
          return "With IoC";
+    }
 
+    @PostMapping("registerUserBody")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User registerUser(@RequestBody User user){
+        System.out.println("User name: "+ user.getUsername());
+        System.out.println("User email: "+ user.getUserEmail());
+        return user;
     }
 }
